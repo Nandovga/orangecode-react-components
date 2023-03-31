83f2403dc69764544f2cce2d5638f0e4f8443c6e
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2023-03-31 16:54:56 -0300

    v1.0.1
    Componente Button

--- example/index.php
+++ example/index.php
@@ -2,9 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Example Library</title>
+    <title>Exemplo de Uso do Pacote</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="container p-3">
+    <?php
+    require "view/button.php"
+    ?>
+</div>
 </body>
 </html>
---
+++ example/view/button.php
@@ -0,0 +1,10 @@
+<div class="col-4">
+    <h3>Buttons</h3>
+    <p>Use os estilos de botão personalizados do Bootstrap para ações em formulários, caixas de diálogo e muito mais com
+        total flexibilidade de personalização.</p>
+    <button type="button" class="btn btn-primary btn-sm">Button-sm</button>
+    <button type="button" class="btn btn-primary">Button</button>
+    <button type="button" class="btn btn-primary btn-lg">Button-lg</button>
+    <p class="mt-2">Para a personalização dos botões basta adicionar a variavel <code class="text-danger fw-bold">$oc-button()</code> com as propriedades de personalização como chave da lista:</p>
+</div>
+
