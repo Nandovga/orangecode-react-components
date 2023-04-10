193050c0af3b70b8366070694443d6a362b33759
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2023-04-10 10:26:05 -0300

    v1.0.5
    Componente Switch

---
+++ src/react/Form/Switch.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {IInputBase} from "../../@types/form";
+import {GET_ICON} from "../../ts/system";
+import {IIcon} from "../../@types/icon";
+
+export interface ISwitch extends IInputBase, IIcon {
+    value: boolean
+    label: string
+}
+
+/**
+ * Componente SwitchBootstrap5
+ * @param props
+ * @constructor
+ */
+const SwitchBootstrap = ({...props}: ISwitch) => {
+    //Configuração do componente
+    let boxClasses: string = !props.boxClasses ? "" : props.boxClasses
+    let fieldClasses: string = !props.fieldClasses ? "" : props.fieldClasses
+
+    /*
+    |--------------------------------------
+    | render() - Renderização do componente
+    |--------------------------------------
+    */
+    return <div className={"box-" + props.box + " " + boxClasses}>
+        <label className="form-label">
+            <i className={GET_ICON(props.iconType) + props.icon}/>
+            {props.legend}
+        </label>
+        <div className="form-check form-switch">
+            <input className={"form-check-input " + fieldClasses}
+                   type="checkbox"
+                   id={props.name}
+                   disabled={props.disabled}
+                   required={props.required}
+                   checked={props.value}
+                   name={props.name}
+                   onChange={event => {
+                       if (props.onChange)
+                           props.onChange(event.target.checked)
+                   }}/>
+            <label className="form-check-label" htmlFor={props.name}>{props.label}</label>
+        </div>
+    </div>
+}
+
+/**
+ * Componente Switch
+ * @param frameworkStyle
+ * @param props
+ * @constructor
+ */
+const Switch = ({frameworkStyle = "bootstrap", ...props}: ISwitch) => {
+    return frameworkStyle === "bootstrap" ? <SwitchBootstrap {...props}/> : <></>
+}
+export default Switch
