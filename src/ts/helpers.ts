ff0a245370817f986767241c39d352331e1e9890
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2023-04-11 11:57:30 -0300

    v1.0.10
    Correção na importação dos pacotes

---
+++ src/ts/helpers.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import {GET_TYPE} from "./system";
+
+/**
+ * Pesquisa o cep de acordo com value
+ * @param value
+ * @constructor
+ */
+export async function GET_CEP(value: string) {
+    let cep = value.length === 0 ? "00000000" : value.replace("-", "")
+    return axios.get("https://viacep.com.br/ws/" + cep + "/json/")
+        .then(res => GET_TYPE<{
+            bairro: string
+            cep: string
+            complemento: string
+            localidade: string
+            logradouro: string
+            uf: string
+        }>(res.data))
+}
--- src/ts/system.ts
+++ src/ts/system.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import $ from "jquery"
 import {IBreakpoint} from "../@types/system";
 import {IIconType} from "../@types/icon";
@@ -11,24 +10,6 @@ import {IIconType} from "../@types/icon";
  */
 export function GET_TYPE<Type>(value: any): Type {
     return value;
-}
-
-/**
- * Pesquisa o cep de acordo com value
- * @param value
- * @constructor
- */
-export async function GET_CEP(value: string) {
-    let cep = value.length === 0 ? "00000000" : value.replace("-", "")
-    return axios.get("https://viacep.com.br/ws/" + cep + "/json/")
-        .then(res => GET_TYPE<{
-            bairro: string
-            cep: string
-            complemento: string
-            localidade: string
-            logradouro: string
-            uf: string
-        }>(res.data))
 }
 
 /**
