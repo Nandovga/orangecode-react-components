a068011280cd2d3b18c0b156053cefb7c1e490d1
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2023-05-10 16:35:03 -0300

    1.1.9-x4
    Ajuste o POST. Bloqueio de vários requisições ao mesmo tempo.

--- src/ts/request.ts
+++ src/ts/request.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, {CancelTokenSource} from "axios";
 import {BASE, TOKEN} from "./config";
 import {globalMessageFieldsClear, globalResponse} from "./response";
 
@@ -7,19 +7,47 @@ import {globalMessageFieldsClear, globalResponse} from "./response";
  * @param route
  * @param body
  * @param form
+ * @param source
  * @constructor
  */
-export const POST = (route: string, body: null | any, form: string = ""): Promise<any> => {
+export const POST = (
+    route: string,
+    body: null | any,
+    form: string = "",
+    source: CancelTokenSource | null = null
+): Promise<any> => {
     return new Promise((resolve, reject) => {
         let url = BASE + (route.substr(0, 1) === "/" ? "" : "/") + route
         globalMessageFieldsClear(form);
+
+        let requestCount = 0;
+        axios.interceptors.request.use(config => {
+            requestCount = requestCount + 1
+            return config;
+        }, error => {
+            return reject(error)
+        })
+
+        axios.interceptors.response.use(response => {
+            requestCount = requestCount - 1
+            return response;
+        }, error => {
+            requestCount = requestCount - 1
+            return reject(error)
+        });
+
         axios({
             method: "post",
             url: url,
             headers: {"X-CSRF-TOKEN": TOKEN},
-            data: !body ? {} : body
+            data: !body ? {} : body,
+            cancelToken: !source ? undefined : source.token
         })
-            .then(response => resolve(response.data))
+            .then(response => {
+                if (source !== null && requestCount > 1)
+                    source.cancel("Muitas requisições sendo executadas!");
+                resolve(response.data)
+            })
             .catch(error => {
                 globalResponse(error.response?.data, form)
                 reject(error)
